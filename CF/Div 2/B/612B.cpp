e1e2a14373c9331a09333d4e952bda0f2e0ec07b
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2018-08-22 00:40:29 +0800

    Create 612B.cpp

---
+++ CF/Div 2/B/612B.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define sf(n) scanf("%d",&n)
+#define pd(n)  printf("%.12lf",n)
+#define pf(n)  printf("%d", n)
+#define p() 	printf("\n")
+#define ps() 	printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define  qu(q)    queue<int>q
+#define pqu(q)    priority_queue<int>q
+#define len(s)   s.length()
+#define sz(x)    x.size()
+#define pb(x)    push_back(x)
+#define all(x)   x.begin(),x.end()
+#define rall(x)  x.rbegin(),x.rend()
+
+pair<int,int>f[200005];
+
+int main(){
+
+    //freopen("input.txt","r",stdin);
+    //freopen("output.tx(t","w",stdout);
+    int n;
+    cin>>n;
+    for(int i=0;i<n;++i){
+        sf(f[i].first);
+        f[i].second=i;
+    }
+    sort(f,f+n);
+    ll ans=0;
+    for(int i=1;i<n;++i){
+        ans+=abs(f[i].second-f[i-1].second);
+    }
+    cout<<ans<<endl;
+}
