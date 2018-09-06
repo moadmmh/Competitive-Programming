fe850eb8e990061dccc5054036d38402841ce7d2
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2018-09-06 20:35:51 +0800

    Create 976A.cpp

---
+++ CF/Div 2/A/976A.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define sf(n) scanf("%d",&n)
+#define pd(n)  printf("%.12lf",n)
+#define pf(n)  printf("%d", n)
+#define p() 	printf("\n")
+#define ps() 	printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define  qu(q)    queue<int>q
+#define pqu(q)    priority_queue<int>q
+#define len(s)   s.length()
+#define sz(x)    x.size()
+#define pb(x)    push_back(x)
+#define all(x)   x.begin(),x.end()
+#define rall(x)  x.rbegin(),x.rend()
+
+
+
+int main() {
+
+    //freopen("input.txt","r",stdin);
+    //freopen("output.tx(t","w",stdout);
+    string s;
+    int n, ones = 0;
+    cin >> n;
+    cin >> s;
+    for (int i = 0; i < n; i += 1) {
+        if (s[i] == '1')ones += 1;
+    }
+    int x=0;
+    if (ones > 0){
+         x = n - ones;
+        cout << 1;
+        while (x--) {
+        cout << 0;
+        }
+        return 0;
+    }
+    cout<<0<<endl;
+
+    return 0;
+}
