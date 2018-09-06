0448df15f1628384b2703225d35c2d71d6853994
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2018-09-06 20:14:50 +0800

    Create 915A.cpp

---
+++ CF/Div 2/A/915A.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define sf(n) scanf("%d",&n)
+#define pd(n)  printf("%.12lf",n)
+#define pf(n)  printf("%d", n)
+#define p() 	printf("\n")
+#define ps() 	printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define  qu(q)    queue<int>q
+#define pqu(q)    priority_queue<int>q
+#define len(s)   s.length()
+#define sz(x)    x.size()
+#define pb(x)    push_back(x)
+#define all(x)   x.begin(),x.end()
+#define rall(x)  x.rbegin(),x.rend()
+
+vector<int>v;
+int main(){
+
+    //freopen("input.txt","r",stdin);
+    //freopen("output.tx(t","w",stdout);
+    int n,k;
+    cin>>n>>k;
+    for(int i=0;i<n;++i){
+        int x;
+        cin>>x;
+        if(k%x==0)
+            v.pb(x);
+    }
+    sort(all(v));
+    cout<<k/v[sz(v)-1]<<endl;
+    return 0;
+}
