a41745748333fe7df70923d2098921e950249936
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2018-09-08 20:26:18 +0800

    Create 937A.cpp

---
+++ CF/Div 2/A/937A.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define sf(n) scanf("%d",&n)
+#define pd(n)  printf("%.12lf",n)
+#define pf(n)  printf("%d", n)
+#define p() 	printf("\n")
+#define ps() 	printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define  qu(q)    queue<int>q
+#define pqu(q)    priority_queue<int>q
+#define len(s)   s.length()
+#define sz(x)    x.size()
+#define pb(x)    push_back(x)
+#define all(x)   x.begin(),x.end()
+#define rall(x)  x.rbegin(),x.rend()
+
+stack<int>st;
+vector<int>v;
+bool a[605]={false};
+
+int main() {
+
+    //freopen("input.txt","r",stdin);
+    //freopen("output.tx(t","w",stdout);
+    int n;
+    cin>>n;
+    for(int i=0;i<n;++i){
+        int x;  sf(x);
+        if(x==0)
+            continue;
+        if(!a[x]){
+            v.pb(x);
+            a[x]=true;
+        }
+    }
+    cout<<sz(v)<<endl;
+
+    return 0;
+}
