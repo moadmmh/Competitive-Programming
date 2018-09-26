73ca7a8a818f07c9a5a58a2c00bef53afe6bb755
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2018-09-26 19:22:44 +0800

    Create 81A.cpp

---
+++ CF/Others/81A.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define sf(n) scanf("%d",&n)
+#define pd(n)  printf("%.12lf",n)
+#define pf(n)  printf("%d", n)
+#define p() 	printf("\n")
+#define ps() 	printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define  qu(q)    queue<int>q
+#define pqu(q)    priority_queue<int>q
+#define len(s)   s.length()
+#define sz(x)    x.size()
+#define pb(x)    push_back(x)
+#define all(x)   x.begin(),x.end()
+#define rall(x)  x.rbegin(),x.rend()
+
+stack<char>st;
+vector<char>v;
+int main() {
+
+    //freopen("input.txt","r",stdin);
+    //freopen("output.tx(t","w",stdout);
+    string s;
+    cin>>s;
+    for(int i=0;i<len(s);++i){
+            if(st.empty()){
+                st.push(s[i]);
+            }
+            else if(st.top()==s[i]){
+                st.pop();
+            }
+            else{
+                st.push(s[i]);
+            }
+    }
+    while(!st.empty()){
+        v.pb(st.top());
+        st.pop();
+    }
+    reverse(all(v));
+    for(int i=0;i<sz(v);++i){
+        cout<<v[i];
+    }
+    p();
+    return 0;
+}
