b135cecdefe01b7102bafae525c6b78791392efa
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2018-10-07 17:40:55 +0800

    Create 598A.cpp

---
+++ CF/Div 2/A/598A.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define sf(n) scanf("%d",&n)
+#define pd(n)  printf("%.12lf",n)
+#define pf(n)  printf("%d", n)
+#define p() 	printf("\n")
+#define ps() 	printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define  qu(q)    queue<int>q
+#define pqu(q)    priority_queue<int>q
+#define len(s)   s.length()
+#define sz(x)    x.size()
+#define pb(x)    push_back(x)
+#define all(x)   x.begin(),x.end()
+#define rall(x)  x.rbegin(),x.rend()
+
+
+int main() {
+
+    //freopen("input.txt","r",stdin);
+    //freopen("output.tx(t","w",stdout);
+    int n,T;
+    cin>>T;
+    while(T--){
+        cin>>n;
+        ll sum=0,tmp=1;
+        sum=((n+1)*1LL*(n))/2;
+        ll x=1;
+        while(x<=n){
+            x=x*2;
+            if(x<=n)
+                tmp+=x;
+        }
+        cout<<sum-(2*1LL*tmp)<<endl;
+
+    }
+    return 0;
+}
