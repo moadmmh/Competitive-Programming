6d978aaf520e6c7126ec06bfc51e68ae8d4e547d
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2018-10-14 22:38:40 +0800

    Create 1065A.cpp

---
+++ CF/Div 2/A/1065A.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define sf(n) scanf("%d",&n)
+#define pd(n)  printf("%.12lf",n)
+#define pf(n)  printf("%d", n)
+#define p() 	printf("\n")
+#define ps() 	printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e12
+#define dbl 1e09
+#define  qu(q)    queue<int>q
+#define pqu(q)    priority_queue<int>q
+#define len(s)   s.length()
+#define sz(x)    x.size()
+#define pb(x)    push_back(x)
+#define all(x)   x.begin(),x.end()
+#define rall(x)  x.rbegin(),x.rend()
+
+int main() {
+
+    //freopen("input.txt","r",stdin);
+    //freopen("output.tx(t","w",stdout);
+
+    int T;
+    cin>>T;
+    while(T--){
+        int s,a,b,c;
+        cin>>s>>a>>b>>c;
+        ll ans=(s/c)*1LL*(1)+ ((s/c)*1LL/(a)*1LL)*1LL*b;
+        cout<<ans<<endl;
+    }
+
+    return 0;
+}
