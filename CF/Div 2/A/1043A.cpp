9c02bf4885d88728e04e4844287f2286d524bdb4
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2018-10-29 04:23:58 +0800

    Create 1043A.cpp

---
+++ CF/Div 2/A/1043A.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define sf(n) scanf("%d",&n)
+#define pd(n)  printf("%.12lf",n)
+#define pf(n)  printf("%d", n)
+#define p() 	printf("\n")
+#define ps() 	printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e12
+#define dbl 1e09
+#define  qu(q)    queue<int>q
+#define pqu(q)    priority_queue<int>q
+#define len(s)   s.length()
+#define sz(x)    x.size()
+#define pb(x)    push_back(x)
+#define all(x)   x.begin(),x.end()
+#define rall(x)  x.rbegin(),x.rend()
+int a[101];
+
+int main() {
+
+    //freopen("input.txt","r",stdin);
+    //freopen("output.tx(t","w",stdout);
+    int n,sum=0,maxi=0;
+    cin>>n;
+    for(int i=0;i<n;++i){
+        cin>>a[i];
+        sum+=a[i];
+        maxi=max(maxi,a[i]);
+    }
+    for(int i=maxi;i<1000;++i){
+        if(n*i>2*sum){
+            cout<<i<<endl;
+            return 0;
+        }
+    }
+    return 0;
+}
