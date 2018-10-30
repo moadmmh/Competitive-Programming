57b72a257070238eb0b032ba6df447523a75e5ee
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2018-10-31 01:08:37 +0800

    Create 255A.cpp

---
+++ CF/Div 2/A/255A.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define sf(n) scanf("%d",&n)
+#define pd(n)  printf("%.12lf",n)
+#define pf(n)  printf("%d", n)
+#define p() 	printf("\n")
+#define ps() 	printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e12
+#define dbl 1e09
+#define  qu(q)    queue<int>q
+#define pqu(q)    priority_queue<int>q
+#define len(s)   s.length()
+#define sz(x)    x.size()
+#define pb(x)    push_back(x)
+#define all(x)   x.begin(),x.end()
+#define rall(x)  x.rbegin(),x.rend()
+int a[5];
+int main() {
+
+    //freopen("input.txt","r",stdin);
+    //freopen("output.tx(t","w",stdout);
+    int n;
+    cin>>n;
+    for(int i=0;i<n;++i){
+        int x;  sf(x);
+        a[i%3]+=x;
+    }
+    if(max(max(a[1],a[2]),a[0])==a[0])
+        cout<<"chest";
+    else if(max(max(a[1],a[2]),a[0])==a[1])
+        cout<<"biceps";
+    else
+        cout<<"back";
+    return 0;
+}
