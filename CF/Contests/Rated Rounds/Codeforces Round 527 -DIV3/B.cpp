50bdacf1b8efcb89e128eff30e9a41fbf34b2a3d
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2018-12-19 01:34:21 +0800

    During Contest

---
+++ CF/Contests/Rated Rounds/Codeforces Round 527 -DIV3/B.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define sf(n) scanf("%d",&n)
+#define pd(n)  printf("%.12lf",n)
+#define pf(n)  printf("%d", n)
+#define p() 	printf("\n")
+#define ps() 	printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define  qu(q)    queue<int>q
+#define pqu(q)    priority_queue<int>q
+#define len(s)   s.length()
+#define sz(x)    x.size()
+#define pb(x)    push_back(x)
+#define all(x)   x.begin(),x.end()
+#define rall(x)  x.rbegin(),x.rend()
+int a[105];
+int main() {
+    //freopen("input.txt","r",stdin);
+    int n;
+    cin>>n;
+    for(int i=0;i<n;++i){
+        cin>>a[i];
+    }
+    int cnt=0;
+    sort(a,a+n);
+    for(int i=1;i<n;i+=2){
+        cnt+=a[i]-a[i-1];
+    }
+    cout<<cnt<<endl;
+    return 0;
+}
