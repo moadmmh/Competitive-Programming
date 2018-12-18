a3e67f3649ff01f39f78f7618f6de602c6594249
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2018-12-19 01:48:32 +0800

    Upsolved (Tutorial)

---
+++ Personal Training/Daily Speed Training/Day 1/1051B.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define sf(n) scanf("%d",&n)
+#define pd(n)  printf("%.12lf",n)
+#define pf(n)  printf("%d", n)
+#define p() 	printf("\n")
+#define ps() 	printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define  qu(q)    queue<int>q
+#define pqu(q)    priority_queue<int>q
+#define len(s)   s.length()
+#define sz(x)    x.size()
+#define pb(x)    push_back(x)
+#define all(x)   x.begin(),x.end()
+#define rall(x)  x.rbegin(),x.rend()
+
+int main() {
+    //freopen("input.txt","r",stdin);
+    ll l,r;
+    cin>>l>>r;
+    cout<<"YES"<<endl;
+    for(ll i=l;i<r;i+=2){
+        cout<<i<<" "<<i+1<<endl;
+    }
+    return 0;
+}
