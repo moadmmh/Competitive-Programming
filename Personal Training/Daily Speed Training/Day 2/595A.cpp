c1b137be1de4f9edd307047172fb267e849846c6
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2018-12-25 03:03:49 +0800

    During Contest

---
+++ Personal Training/Daily Speed Training/Day 2/595A.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define sf(n) scanf("%d",&n)
+#define pd(n)  printf("%.12lf",n)
+#define pf(n)  printf("%d", n)
+#define p() 	printf("\n")
+#define ps() 	printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define  qu(q)    queue<int>q
+#define pqu(q)    priority_queue<int>q
+#define len(s)   s.length()
+#define sz(x)    x.size()
+#define pb(x)    push_back(x)
+#define all(x)   x.begin(),x.end()
+#define rall(x)  x.rbegin(),x.rend()
+int a[105][205];
+int main() {
+    //freopen("input.txt","r",stdin);
+    int n,m;
+    cin>>n>>m;
+    for(int i=0;i<n;++i){
+        for(int j=0;j<(2*m);++j){
+            cin>>a[i][j];
+        }    
+    }
+    int cnt=0;
+    for(int i=0;i<n;++i){
+        for(int j=0;j<(2*m)-1;j+=2){
+            if(a[i][j]==1 or a[i][j+1]==1)
+                cnt+=1;
+        }    
+    }
+    cout<<cnt<<endl;
+    return 0;
+}
