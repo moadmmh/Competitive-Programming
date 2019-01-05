b83d2a2c8852f4bcac8de94a761ac2430788f600
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2019-01-05 16:07:15 +0800

    Create 265A.cpp

---
+++ CF/Div 2/A/265A.cpp
@@ -0,0 +1,38 @@
+
+#include<bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+
+#define sf(n) scanf("%d",&n)
+#define pd(n)  printf("%.12lf",n)
+#define pf(n)  printf("%d\n", n)
+#define p() 	printf("\n")
+#define ps() 	printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define  qu(q)    queue<int>q
+#define pqu(q)    priority_queue<int>q
+#define len(s)   s.length()
+#define sz(x)    x.size()
+#define pb(x)    push_back(x)
+#define all(x)   x.begin(),x.end()
+#define rall(x)  x.rbegin(),x.rend()
+
+int main() {
+    //freopen("input.txt","r",stdin);
+    string s,t;
+    cin>>s>>t;
+    int pos=0;
+    for(int i=0;i<sz(t);++i){
+        if(t[i]==s[pos]){
+            pos+=1;
+        }
+    }   
+    cout<<pos+1<<endl;
+    return 0;
+}
