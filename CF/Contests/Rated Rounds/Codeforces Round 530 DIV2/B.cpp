10c84ba5850c0af66328421caafd49e2803c7e34
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2019-01-06 03:59:35 +0800

    Create B.cpp

---
+++ CF/Contests/Rated Rounds/Codeforces Round 530 DIV2/B.cpp
@@ -0,0 +1,40 @@
+
+#include<bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+
+#define sf(n) scanf("%d",&n)
+#define pd(n)  printf("%.12lf",n)
+#define pf(n)  printf("%d\n", n)
+#define p() 	printf("\n")
+#define ps() 	printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define  qu(q)    queue<int>q
+#define pqu(q)    priority_queue<int>q
+#define len(s)   s.length()
+#define sz(x)    x.size()
+#define pb(x)    push_back(x)
+#define all(x)   x.begin(),x.end()
+#define rall(x)  x.rbegin(),x.rend()
+
+int main() {
+    //freopen("input.txt","r",stdin);
+    int n;
+    cin>>n;
+    int h=1,v=1;
+    while(h*v<n){
+        if(h<=v)
+            h+=1;
+        else 
+            v+=1;
+        //cout<<h<<" "<<v<<endl;
+    }
+    cout<<h+v<<endl;
+    return 0;
+}
