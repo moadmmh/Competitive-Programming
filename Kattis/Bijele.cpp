0658fc59231c8b6f3c0bd214d45398cdca7e6180
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2019-01-17 04:54:51 +0800

    Create Bijele.cpp

---
+++ Kattis/Bijele.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+
+#define sf(n) scanf("%d",&n)
+#define pd(n)  printf("%.12lf",n)
+#define pf(n)  printf("%d\n", n)
+#define p() 	printf("\n")
+#define ps() 	printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define  qu(q)    queue<int>q
+#define pqu(q)    priority_queue<int>q
+#define len(s)   (int)s.length()
+#define sz(x)    (int)x.size()
+#define pb(x)    push_back(x)
+#define all(x)   x.begin(),x.end()
+#define rall(x)  x.rbegin(),x.rend()
+
+int main() {
+    //freopen("input.txt","r",stdin);
+    int a[6]={1,1,2,2,2,8};
+    for(int i=0;i<6;++i){
+        int x;  sf(x);
+        cout<<a[i]-x<<" ";
+    }
+    p();
+    
+    return 0;
+}   
