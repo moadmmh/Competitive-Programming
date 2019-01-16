f9c6b909da37cfe43e4c6ed65e531a7d5e47be46
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2019-01-16 22:30:26 +0800

    Create 515A.cpp

---
+++ CF/Div 2/A/515A.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+
+#define sf(n) scanf("%d",&n)
+#define pd(n)  printf("%.12lf",n)
+#define pf(n)  printf("%d\n", n)
+#define p() 	printf("\n")
+#define ps() 	printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define  qu(q)    queue<int>q
+#define pqu(q)    priority_queue<int>q
+#define len(s)   (int)s.length()
+#define sz(x)    (int)x.size()
+#define pb(x)    push_back(x)
+#define all(x)   x.begin(),x.end()
+#define rall(x)  x.rbegin(),x.rend()
+
+
+int main() {
+    //freopen("input.txt","r",stdin);
+    int a,b,s;
+    cin>>a>>b>>s;
+    a=abs(a);
+    b=abs(b);
+    if(a+b<=s and (a+b==s or (s-(a+b))%2==0) ){
+        return cout<<"Yes"<<endl,0;
+    }
+    cout<<"No"<<endl;
+    return 0;
+}   
