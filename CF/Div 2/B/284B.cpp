01184d3a838303e792f540ca8af6ad031d1369f6
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2019-01-16 22:30:50 +0800

    Create 284B.cpp

---
+++ CF/Div 2/B/284B.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+
+#define sf(n) scanf("%d",&n)
+#define pd(n)  printf("%.12lf",n)
+#define pf(n)  printf("%d\n", n)
+#define p() 	printf("\n")
+#define ps() 	printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define  qu(q)    queue<int>q
+#define pqu(q)    priority_queue<int>q
+#define len(s)   (int)s.length()
+#define sz(x)    (int)x.size()
+#define pb(x)    push_back(x)
+#define all(x)   x.begin(),x.end()
+#define rall(x)  x.rbegin(),x.rend()
+
+
+int main() {
+    //freopen("input.txt","r",stdin);
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    int I=0,F=0,A=0;
+    for(auto ch:s){
+        if(ch=='A')
+            A+=1;
+        else if(ch=='I')
+            I+=1;
+        else 
+            F+=1;
+    }
+    if(I==0){
+        cout<<A<<endl;return 0;
+    }
+    if(I>0){
+        if(I==1)
+            cout<<1<<endl;
+        else 
+            cout<<0<<endl;
+    }
+    return 0;
+}   
