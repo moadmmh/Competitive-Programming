4341f2dc26494ee389af35e1cb22e8082ca75a86
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2019-01-17 16:33:10 +0800

    Create Different Distances.cpp

---
+++ Kattis/Different Distances.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+
+#define sf(n) scanf("%d",&n)
+#define pd(n)  printf("%.12lf",n)
+#define pf(n)  printf("%d\n", n)
+#define p() 	printf("\n")
+#define ps() 	printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define  qu(q)    queue<int>q
+#define pqu(q)    priority_queue<int>q
+#define len(s)   (int)s.length()
+#define sz(x)    (int)x.size()
+#define pb(x)    push_back(x)
+#define all(x)   x.begin(),x.end()
+#define rall(x)  x.rbegin(),x.rend()
+
+int main() {
+    //freopen("input.txt","r",stdin);
+    double x1,x2,y1,y2,p;
+    while(cin>>x1 and x1){
+        cin>>y1>>x2>>y2>>p;
+        cout<<fixed<<setprecision(9)<<pow(pow(abs(x1-x2),p)+pow(abs(y1-y2),p),1/p)<<endl;
+    }
+    
+    return 0;
+}   
