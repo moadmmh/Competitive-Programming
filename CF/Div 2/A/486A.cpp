1ff0ebc528f286e1b3e050dd3aef0d3759bdcf8e
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2019-01-18 18:47:33 +0800

    Create 486A.cpp

---
+++ CF/Div 2/A/486A.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+
+#define sf(n) scanf("%d",&n)
+#define pd(n)  printf("%.12lf",n)
+#define pf(n)  printf("%d\n", n)
+#define p() 	printf("\n")
+#define ps() 	printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define  qu(q)    queue<int>q
+#define pqu(q)    priority_queue<int>q
+#define len(s)   (int)s.length()
+#define sz(x)    (int)x.size()
+#define pb(x)    push_back(x)
+#define all(x)   x.begin(),x.end()
+#define rall(x)  x.rbegin(),x.rend()
+
+int main() {
+    //freopen("input.txt","r",stdin);
+    ll n;
+    cin>>n;
+    ll tmp=0,tmp1=0;
+    tmp=(n&1)?((n+1)/2):(n/2);
+    tmp1=n-tmp;
+    
+    cout<<tmp1*1LL*(tmp1+1)-1LL*(tmp*1LL*tmp)<<endl;
+    return 0;
+}   
