f24cab30da7e1cb9a931f0b33110d1c3d1bd4acc
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2019-01-18 19:12:29 +0800

    Create 53C.cpp

---
+++ CF/Div 2/C/53C.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+
+#define sf(n) scanf("%d",&n)
+#define pd(n)  printf("%.12lf",n)
+#define pf(n)  printf("%d\n", n)
+#define p() 	printf("\n")
+#define ps() 	printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define  qu(q)    queue<int>q
+#define pqu(q)    priority_queue<int>q
+#define len(s)   (int)s.length()
+#define sz(x)    (int)x.size()
+#define pb(x)    push_back(x)
+#define all(x)   x.begin(),x.end()
+#define rall(x)  x.rbegin(),x.rend()
+
+int a[1005];
+
+int main() {
+    //freopen("input.txt","r",stdin);
+    int n;
+    cin>>n;
+    int x=n-1,tmp=n-1,curr=1;
+    bool ok=true;
+    cout<<1<<' ';
+    while(x--){
+        if(ok){
+            cout<<curr+tmp<<' ';
+            curr+=tmp;
+            tmp--;
+            ok=!ok;
+        }
+        else{
+            cout<<curr-tmp<<' ';
+            curr-=tmp;
+            tmp--;
+            ok=!ok;
+        }
+    }
+    p();
+    return 0;
+}   
