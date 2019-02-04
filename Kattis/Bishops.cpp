a32fee8441c8178f900204e30e8abf2af21df011
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2019-02-05 01:09:01 +0800

    Create Bishops.cpp

---
+++ Kattis/Bishops.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+
+#define sf(n) scanf("%d", &n)
+#define pd(n) printf("%.12lf", n)
+#define pf(n) printf("%d\n", n)
+#define p() printf("\n")
+#define ps() printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define qu(q) queue<int> q
+#define pqu(q) priority_queue<int> q
+#define len(s) (int)s.length()
+#define sz(x) (int)x.size()
+#define pb(x) push_back(x)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    int n;
+    while (cin >> n)
+    {
+        if (n == 0 or n == 1)
+            cout << n << endl;
+        else
+            cout << 2 * n - 2 << endl;
+    }
+    return 0;
+}
