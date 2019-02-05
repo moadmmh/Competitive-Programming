1fa47d27bd3894bf18b590e4596aec802b63cc3e
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2019-02-06 07:58:38 +0800

    Create Soda Surpler.cpp

---
+++ Kattis/Soda Surpler.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+
+#define sf(n) scanf("%d", &n)
+#define pd(n) printf("%.12lf", n)
+#define pf(n) printf("%d\n", n)
+#define p() printf("\n")
+#define ps() printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define qu(q) queue<int> q
+#define pqu(q) priority_queue<int> q
+#define len(s) (int)s.length()
+#define sz(x) (int)x.size()
+#define pb(x) push_back(x)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    int e, f, c;
+    cin >> e >> f >> c;
+    int st = e + f, ans = st / c, rst = (st % c) + ans;
+    int res = ans;
+    while (rst >= c)
+    {
+        res += rst / c;
+        rst = (rst / c) + rst % c;
+    }
+    cout << res << endl;
+    return 0;
+}
