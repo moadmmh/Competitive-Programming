cb2ed0d924b10e8c1f0aa7866cc16bb866d82fb1
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2019-02-09 17:43:11 +0800

    Create 1030B.cpp

---
+++ CF/Technocup 2019/1030B.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+
+#define sf(n) scanf("%d", &n)
+#define pd(n) printf("%.12lf", n)
+#define pf(n) printf("%d\n", n)
+#define p() printf("\n")
+#define ps() printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define qu(q) queue<int> q
+#define pqu(q) priority_queue<int> q
+#define len(s) (int)s.length()
+#define sz(x) (int)x.size()
+#define pb(x) push_back(x)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    int n, d;
+    cin >> n >> d;
+    int m;
+    cin >> m;
+    int x, y;
+    while (m--)
+    {
+        cin >> x >> y;
+        if ((x + y <= 2 * n - d and x + y >= d) and (x - y >= -d and x - y <= d))
+            cout << "YES" << endl;
+        else
+            cout << "NO" << endl;
+    }
+    return 0;
+}
