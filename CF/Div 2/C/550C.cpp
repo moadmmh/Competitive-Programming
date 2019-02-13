4803dbfaef14e72d06da19e6fb80393cfb1d61b1
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2019-02-13 20:37:44 +0800

    Create 550C.cpp

---
+++ CF/Div 2/C/550C.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+
+#define sf(n) scanf("%d", &n)
+#define pd(n) printf("%.12lf", n)
+#define pf(n) printf("%d\n", n)
+#define p() printf("\n")
+#define ps() printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define qu(q) queue<int> q
+#define pqu(q) priority_queue<int> q
+#define len(s) (int)s.length()
+#define sz(x) (int)x.size()
+#define pb(x) push_back(x)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    string s;
+    cin >> s;
+    s = "00" + s;
+    for (int i = 0; i < sz(s) - 2; ++i)
+    {
+        for (int j = i + 1; j < sz(s) - 1; ++j)
+        {
+            for (int k = j + 1; k < sz(s); ++k)
+            {
+                int tmp = 100 * (s[i] - '0') + 10 * (s[j] - '0') + (s[k] - '0');
+                if (tmp % 8 == 0)
+                {
+                    cout << "YES" << endl;
+                    return cout << tmp << endl, 0;
+                }
+            }
+        }
+    }
+    cout << "NO" << endl;
+    return 0;
+}
