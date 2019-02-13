1100fff331714da8d12ed375b61a55f1cb25790c
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2019-02-13 21:33:46 +0800

    Create Pot.cpp

---
+++ Kattis/Pot.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+
+#define sf(n) scanf("%d", &n)
+#define pd(n) printf("%.12lf", n)
+#define pf(n) printf("%d\n", n)
+#define p() printf("\n")
+#define ps() printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define qu(q) queue<int> q
+#define pqu(q) priority_queue<int> q
+#define len(s) (int)s.length()
+#define sz(x) (int)x.size()
+#define pb(x) push_back(x)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    int n;
+    cin >> n;
+    ll sum = 0;
+    while (n--)
+    {
+        int x;
+        cin >> x;
+        int p = x % 10;
+        x /= 10;
+        sum += pow(x, p);
+    }
+    cout << sum << endl;
+}
