d34071884c33d4d935e00ff1926461472a5c2b2e
Moad Mohammed Elhebri <37096896+moadmmh@users.noreply.github.com>
2019-02-13 21:27:19 +0800

    Create Quadrant Selection.cpp

---
+++ Kattis/Quadrant Selection.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+
+#define sf(n) scanf("%d", &n)
+#define pd(n) printf("%.12lf", n)
+#define pf(n) printf("%d\n", n)
+#define p() printf("\n")
+#define ps() printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define qu(q) queue<int> q
+#define pqu(q) priority_queue<int> q
+#define len(s) (int)s.length()
+#define sz(x) (int)x.size()
+#define pb(x) push_back(x)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    int x, y;
+    cin >> x >> y;
+    if (x < 0 and y < 0)
+        cout << 3 << endl;
+    else if (x < 0 and y > 0)
+        cout << 2 << endl;
+    else if (x > 0 and y < 0)
+        cout << 4 << endl;
+    else
+        cout << 1 << endl;
+    return 0;
+}
