d2612c49f4524882a4fdba4ded48383b816e7a1b
moadmmh <moad.elhebri@live.iium.edu.my>
2019-02-17 19:59:16 +0800

    637B.cpp

---
+++ CF/Div 2/B/637B.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sf(n) scanf("%d", &n)
+#define pd(n) printf("%.12lf", n)
+#define pf(n) printf("%d", n)
+#define p() printf("\n")
+#define ps() printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000000000
+#define pi 3.1415926536
+#define ex 1e9
+#define dbl 1e09
+#define qu(q) queue<int> q
+#define pqu(q) priority_queue<int> q
+#define len(s) s.length()
+#define sz(x) x.size()
+#define pb(x) push_back(x)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+set<string> st;
+vector<string> v;
+map<string, int> taken;
+
+int main()
+{
+    //freopen("input.txt","r",stdin);
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i)
+    {
+        string s;
+        cin >> s;
+        v.pb(s);
+    }
+    for (int i = n - 1; i >= 0; --i)
+    {
+        if (!taken[v[i]]){
+            cout << v[i] << endl;
+            taken[v[i]] = 1;
+        }
+    }
+    return 0;
+}
