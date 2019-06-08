af7e6523774b256d61c6a15e3832f19b1bc9dcb0
moadmmh <moad.elhebri@live.iium.edu.my>
2019-06-08 10:32:35 +0800

    practice

---
+++ CF/Gym/Ramadan Marathon by ICPC Assiut/B0.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define sf(n) scanf("%d", &n)
+#define pd(n) printf("%.12lf", n)
+#define pf(n) printf("%d", n)
+#define p() printf("\n")
+#define ps() printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000 * 1000 * 1000 + 7
+#define pi 3.1415926536
+#define ex 1e18
+#define dbl 2e09
+#define qu(q) queue<int> q
+#define pqu(q) priority_queue<int> q
+#define len(s) s.length()
+#define sz(x) int(x.size())
+#define pb(x) push_back(x)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+int main()
+{
+    int n, x, y, z;
+    cin >> n >> x >> y >> z;
+    int ans1 = (n / x) * y + (n % x) * z;
+    int ans2 = n * z;
+    cout << min(ans1, ans2) << endl;
+    return 0;
+}
