8de4dd79d679c7223fc4ef8031c8336f2750fc29
moadmmh <moad.elhebri@live.iium.edu.my>
2019-06-11 02:24:42 +0800

    practice

---
+++ CF/Gym/Ramadan Marathon by ICPC Assiut/K0.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define sf(n) scanf("%d", &n)
+#define pd(n) printf("%.12lf", n)
+#define pf(n) printf("%d", n)
+#define p() printf("\n")
+#define ps() printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000 * 1000 * 1000 + 7
+#define pi 3.1415926536
+#define ex 1e18
+#define dbl 2e09
+#define qu(q) queue<int> q
+#define pqu(q) priority_queue<int> q
+#define len(s) s.length()
+#define sz(x) int(x.size())
+#define pb(x) push_back(x)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3f;
+
+int main()
+{
+    int T;
+    cin >> T;
+    int cs = 1;
+    while (T--)
+    {
+        double x, y, z;
+        cin >> x >> y >> z;
+        double av=(x+y+z)/3.0;
+        cout << "Case " << cs << ": ";
+        printf("%.6f",av);
+        p();
+        cs+=1;
+    }
+    return 0;
+}
