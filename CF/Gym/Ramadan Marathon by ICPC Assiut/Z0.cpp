c78d81e972add0162635472445d60ef1e7e6dc72
moadmmh <moad.elhebri@live.iium.edu.my>
2019-06-11 10:53:12 +0800

    practice

---
+++ CF/Gym/Ramadan Marathon by ICPC Assiut/Z0.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define sf(n) scanf("%d", &n)
+#define pd(n) printf("%.12lf", n)
+#define pf(n) printf("%d", n)
+#define p() printf("\n")
+#define ps() printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000 * 1000 * 1000 + 7
+#define pi 3.1415926536
+#define ex 1e18
+#define dbl 2e09
+#define qu(q) queue<int> q
+#define pqu(q) priority_queue<int> q
+#define len(s) s.length()
+#define sz(x) int(x.size())
+#define pb(x) push_back(x)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3f;
+
+ll a[100005];
+ll dp[10005];
+int n;
+
+int main()
+{
+
+    cin >> n;
+    memset(dp, -1, sizeof dp);
+    for (int i = 0; i < n; ++i)
+        cin >> a[i];
+
+    ll ans = a[0];
+    ll tmp = ans;
+    for (int i = 1; i < n; ++i)
+    {
+        tmp = max(a[i], a[i] + tmp);
+        ans = max(ans, tmp);
+    }
+    cout << ans << endl;
+    return 0;
+}
