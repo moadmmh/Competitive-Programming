52e18746c2e5d10f1da1b65e1885668a8767038b
moadmmh <moad.elhebri@live.iium.edu.my>
2019-07-03 05:26:57 +0800

    Day 3 Atcoder contest

---
+++ Atcoder/abc124/A.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define sf(n) scanf("%d", &n)
+#define pd(n) printf("%.12lf", n)
+#define pf(n) printf("%d", n)
+#define p() printf("\n")
+#define ps() printf(" ")
+#define ll long long
+#define ull unsigned long long
+#define MAX 1e9
+#define mod 1000 * 1000 * 1000 + 7
+#define pi 3.1415926536
+#define ex 1e18
+#define dbl 2e09
+#define qu(q) queue<int> q
+#define pqu(q) priority_queue<int> q
+#define len(s) s.length()
+#define sz(x) int(x.size())
+#define pb(x) push_back(x)
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3f;
+
+ll a, b;
+string s;
+
+int ra[1000010], rb[1000010];
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    cout << max(a + b, max(2 * a - 1, 2 * b - 1)) << endl;
+    return 0;
+}
